510a287cf13d65d536a6e64f2c5715d364076a80
eperkhalsky <egor.perkhalsky@amasty.com>
2023-01-31 15:24:56 +0300

    add scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,33 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     const typescriptLoader = {
             test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/,
         };
 
+    const cssLoader = {
+      test: /\.s[ac]ss$/i,
+      use: [
+          options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+              loader: "css-loader",
+              options: {
+                  modules: {
+                      auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                      localIdentName: options.isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]'
+                  }
+              }
+          },
+          "sass-loader"
+      ]
+    };
+
     return [
-        typescriptLoader
+        typescriptLoader,
+        cssLoader
     ]
 }
---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Counter} from "./Counter";
+import './index.scss'
+
+const App = () => {
+    return (
+        <div className="app">
+            sdfsdfsdf
+            <Counter />
+        </div>
+    );
+};
+
+export default App;

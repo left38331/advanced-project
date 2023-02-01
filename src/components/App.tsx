2aeaa22618b2d9b58fad618cb2180d9cf1f7995e
eperkhalsky <egor.perkhalsky@amasty.com>
2023-02-01 11:25:40 +0300

    add react dom routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import {Counter} from "./Counter";
 import './index.scss'
+import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "../pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            sdfsdfsdf
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
+            {/*<Counter />*/}
         </div>
     );
 };

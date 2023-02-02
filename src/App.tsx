50bf62aa04f0eb0206e5e7ee0802ea67c302aaea
eperkhalsky <egor.perkhalsky@amasty.com>
2023-02-02 09:41:35 +0300

    improve css class toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import {Counter} from "./Counter";
-import './index.scss'
-import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "../pages/MainPage/MainPage.async";
+import './styles/index.scss'
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
 
@@ -17,7 +21,6 @@ const App = () => {
                     <Route path={'/'} element={<MainPageAsync />} />
                 </Routes>
             </Suspense>
-            {/*<Counter />*/}
         </div>
     );
 };

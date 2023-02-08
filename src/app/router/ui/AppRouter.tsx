7e77a9911037a8f3035d8cfc6977384832f30951
eperkhalsky <egor.perkhalsky@amasty.com>
2023-02-08 13:53:31 +0300

    add sidebar

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -6,17 +6,20 @@ import {routeConfig} from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({element, path}) => (
-                  <Route
-                      key={path}
-                      path={path}
-                      element={element}
-                  />
-                ))}
-            </Routes>
-        </Suspense>
+        <div className='page-wrapper'>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {Object.values(routeConfig).map(({element, path}) => (
+                        <Route
+                            key={path}
+                            path={path}
+                            element={element}
+                        />
+                    ))}
+                </Routes>
+            </Suspense>
+        </div>
+
     );
 };
 

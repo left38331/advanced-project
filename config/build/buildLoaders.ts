c0542066d893c9486297c07bf0f822a4058c5047
eperkhalsky <egor.perkhalsky@amasty.com>
2023-02-12 10:04:40 +0300

    add babel config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,26 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                   [
+                       "i18next-extract",
+                       {
+                            "locales": ["en", "ru"],
+                           keyAsDefaultValue: true
+                       }
+                   ]
+                ]
+            }
+        }
+    }
+
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
         use: [
@@ -43,7 +63,8 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
-        cssLoader
+        cssLoader,
     ]
 }

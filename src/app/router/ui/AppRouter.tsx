eed55ed21f9a8a29a372d31726434a2538f4809e
eperkhalsky <egor.perkhalsky@amasty.com>
2023-02-17 11:58:46 +0300

    add no route page and css loader

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -1,12 +1,11 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader';
 
 const AppRouter = () => (
     <div className="page-wrapper">
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<PageLoader />}>
             <Routes>
                 {Object.values(routeConfig).map(({ element, path }) => (
                     <Route
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoute {
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 }
 
 export const RoutePaths: Record<AppRoute, string> = {
     [AppRoute.MAIN]: '/',
     [AppRoute.ABOUT]: '/about',
+    [AppRoute.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoute, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoute, RouteProps> = {
     [AppRoute.ABOUT]: {
         path: RoutePaths.about,
         element: <AboutPage />,
+    },
+    [AppRoute.NOT_FOUND]: {
+        path: RoutePaths.not_found,
+        element: <NotFoundPage />,
     },
 };

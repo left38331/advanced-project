9d6644ba57a6f5828c499e864c0a51abbe9b6a53
eperkhalsky <egor.perkhalsky@amasty.com>
2023-04-03 14:18:05 +0300

    add husky for pre-commit

---
+++ src/shared/ui/Input/ui/Input.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Input } from './Input';
+
+export default {
+    title: 'share/Input',
+    component: Input,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    placeholder: 'Type text',
+    value: '123',
+};

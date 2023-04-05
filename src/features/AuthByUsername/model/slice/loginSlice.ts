6d127fbb472d580b7363d5a8c9e77fbaf2cb5c7e
eperkhalsky <egor.perkhalsky@amasty.com>
2023-04-05 14:29:14 +0300

    add async module loading, add async reducers manager

--- src/features/AuthByUsername/model/slice/loginSlice.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -20,10 +20,10 @@ export const loginSlice = createSlice({
         },
     },
     extraReducers: (builder) => {
-        builder.addCase(loginByUsername.pending, (state, action) => {
+        builder.addCase(loginByUsername.pending, (state) => {
             state.error = undefined;
             state.isLoading = true;
-        }).addCase(loginByUsername.fulfilled, (state, action) => {
+        }).addCase(loginByUsername.fulfilled, (state) => {
             state.isLoading = false;
         }).addCase(loginByUsername.rejected, (state, action) => {
             state.isLoading = false;
---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,49 @@
+import { FC, ReactNode, useEffect } from 'react';
+import { useDispatch, useStore } from 'react-redux';
+import { ReduxStoreWithManager, StateSchemaKeys } from 'app/providers/StoreProvider/config/StateSchema';
+import { Reducer } from '@reduxjs/toolkit';
+
+export type ReducerList = {
+    [name in StateSchemaKeys]?: Reducer;
+}
+
+type ReducerListEntry = [StateSchemaKeys, Reducer];
+
+interface DynamicModuleLoaderProps {
+    reducers: ReducerList;
+    children: ReactNode;
+    removeAfterUnmount?: boolean;
+}
+
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props: DynamicModuleLoaderProps) => {
+    const {
+        children, reducers, removeAfterUnmount,
+    } = props;
+
+    const store = useStore() as ReduxStoreWithManager;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        Object.entries(reducers).forEach(([name, reducer]: ReducerListEntry) => {
+            store.reducerManager.add(name, reducer);
+            dispatch({ type: `@INIT ${name} reducer` });
+        });
+
+        return () => {
+            if (removeAfterUnmount) {
+                Object.entries(reducers).forEach(([name, reducer]: ReducerListEntry) => {
+                    store.reducerManager.remove(name);
+                    dispatch({ type: `@DESTROY ${name} reducer` });
+                });
+            }
+        };
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        // eslint-disable-next-line react/jsx-no-useless-fragment
+        <>
+            {children}
+        </>
+    );
+};

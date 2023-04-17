7ae268dbec01567d4606afc43d1ed6601328e73c
eperkhalsky <egor.perkhalsky@amasty.com>
2023-04-17 10:49:42 +0300

    add ts ignore

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -12,10 +12,13 @@ export default ({ config }: {config: webpack.Configuration}) => {
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
 
+    // @ts-ignore
     config.resolve.modules.push(paths.src);
+    // @ts-ignore
     config.resolve.extensions.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
+    // @ts-ignore
     config.module.rules = config.module.rules.map((rule: webpack.RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
@@ -24,9 +27,12 @@ export default ({ config }: {config: webpack.Configuration}) => {
         return rule;
     });
 
+    // @ts-ignore
     config.module.rules.push(buildSvgLoader());
+    // @ts-ignore
     config.module.rules.push(buildCssLoader(true));
 
+    // @ts-ignore
     config.plugins.push(new webpack.DefinePlugin({
         __IS_DEV__: true,
         __API__: JSON.stringify(''),
